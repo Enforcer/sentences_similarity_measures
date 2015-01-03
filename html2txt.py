5a96903ac2c1a54bdd9c0abf1607c29f562ab9b0
Sebastian Buczyński <nnplaya@gmail.com>
2015-01-03 19:58:52 +0100

    Poprawki w skrypcie do wyciagania tekstu z htmla

--- html2txt.py
+++ html2txt.py
@@ -2,12 +2,32 @@ import sys
 import os
 from bs4 import BeautifulSoup
 
+
 def get_contents_for_file(filename):
     soup = BeautifulSoup(open(filename, encoding='iso-8859-2'))
     result = soup.get_text()
 
     lines = [l for l in result.split('\n') if l != '' and not l.startswith('Opracowanie Centrum Nowych Technologii') and not l.startswith('Pytania i uwagi dotyczące archiwum')]
-    result = '\n'.join(lines)
+
+    # Usuwanie metainformacji
+    lines2 = []
+    omit = False
+    for line in lines:
+        if line.strip().startswith('Archiwum ROL:') or omit and not line.strip().startswith('Dział'):
+            omit = True
+            continue
+        elif line.strip().startswith('Dział'):
+            omit = False
+            continue
+        lines2.append(line)
+
+    # Usuniecie incjalow autora - za krotkie, by byc uzyteczne, a wprowadza niepotrzebny szum
+    if len(lines2) > 0:
+        lines2.pop()
+    else:
+        print("Empty file: ", filename)
+
+    result = '\n'.join(lines2)
     result = [r for r in result.split() if r]
     return ' '.join(result)
 
@@ -28,6 +48,7 @@ if __name__ == '__main__':
 
     f = open('result.txt', 'w')
     for fcontent in all_contents:
-        f.write(fcontent + '\n')
+        if fcontent.strip():
+            f.write(fcontent + '\n')
     f.close()
 
